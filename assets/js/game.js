e75365a0bd433a2b020cd75ae81842ca53c42197
forsythrl <tarrantrl@utexas.edu>
2020-08-10 12:37:15 -0500

    add fight function and playerName variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,36 @@
-window.alert("This is an alert! JavaScript is running!");
+// Use camel case for variables, JS can't do hyphens
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+// create a function called "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
+
+// The game will prompt the user to name their robot.
+
+// The player's robot will be initialized with the following properties:
+    // 100 health points
+    // 10 attack points
+    // 10 money points
+
+// The player's opponent, Roborto, will be initialized with the following properties:
+    // 50 health points
+    // 12 attack points
+
+// The game will display "Welcome to Robot Gladiators!"
+
+// The game will prompt the user to either fight the round or skip it.
+
+// If the player chooses to skip:
+    // A penalty of 10 money points will be deducted from the player's robot.
+    // The game will end.
+
+// If the player chooses to fight:
+    // The player's robot will attack Roborto, and the player robot's attack points will be deducted from Roborto's health points.
+    // The game will display Roborto's remaining health points.
+    // Roborto will attack the player's robot, and Roberto's attack points will be deducted from the player's robot's health points.
+    // The game will display the player robot's remaining health points.
+
+// The game will end.
