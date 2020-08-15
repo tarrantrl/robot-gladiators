af4bc6e6d2872aba06fc025b3ff781499a2b0f1f
forsythrl <tarrantrl@utexas.edu>
2020-08-15 13:13:58 -0500

    allow shop options to be chosen with integer instead of string input

--- assets/js/game.js
+++ assets/js/game.js
@@ -149,18 +149,18 @@ var endGame = function() {
 var shop = function() {
     //console.log("entered shop");
     // ask player what they'd like to do
-    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
-    shopOptionPrompt = shopOptionPrompt.toLowerCase();
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3  for LEAVE to make a choice.");
+    //shopOptionPrompt = shopOptionPrompt.toLowerCase();
     // use switch to decide what to do with the shop prompt
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt){
-        case "refill": // this is what you do instead of || since there is no other declaration, this will fall through to the next case until there is a break
-        case "REFILL":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "leave":
+        case 3:
             window.alert("Leaving the store");
             break;
         default:
